29cb50a3d74272c632899f703d59ed5bcc831a6b
TudorMorosanu <tudormorosanu20@gmail.com>
2025-01-02 21:37:36 +0200

    implemented controllers and made them launch automatically

--- src/main/java/LanesIntersection/MainModel.java
+++ src/main/java/LanesIntersection/MainModel.java
@@ -2,12 +2,20 @@ package LanesIntersection;
 
 import Components.PetriNet;
 import Components.PetriTransition;
+import Controllers.*;
 
 public class MainModel {
 
-    public MainModel(){
+    public static void main(String[] args){
 
         PetriNet pn = new PetriNet();
+
+        ControllerRebreanu.start();
+        ControllerMacedonski.start();
+        ControllerVuia1.start();
+        ControllerVuia2.start();
+        ControllerUlpia.start();
+        ControllerDrubeta.start();
     }
 
 }
