c91cfece067b558830154446643c5a57ae420316
TudorMorosanu <tudormorosanu20@gmail.com>
2025-01-03 14:28:10 +0200

    created LanesStarter class which starts the whole app

--- src/main/java/LanesIntersection/MainModel.java
+++ src/main/java/LanesIntersection/MainModel.java
@@ -2,20 +2,12 @@ package LanesIntersection;
 
 import Components.PetriNet;
 import Components.PetriTransition;
-import Controllers.*;
 
 public class MainModel {
 
-    public static void main(String[] args){
+    public static void start(){
 
         PetriNet pn = new PetriNet();
-
-        ControllerRebreanu.start();
-        ControllerMacedonski.start();
-        ControllerVuia1.start();
-        ControllerVuia2.start();
-        ControllerUlpia.start();
-        ControllerDrubeta.start();
     }
 
 }
