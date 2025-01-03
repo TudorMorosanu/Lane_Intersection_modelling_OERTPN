179445584806a8411cd1dbdf68658894a54e004d
Alex Cetanas <116481552+Alex-cet@users.noreply.github.com>
2025-01-03 21:00:56 +0200

    Implemented all intersections (#3)
    -Implemented all Petri Nets from all the intersections.
    -Updated the Main Model to call each of the Intersection 'generate' function.

--- src/main/java/LanesIntersection/MainModel.java
+++ src/main/java/LanesIntersection/MainModel.java
@@ -1,13 +1,32 @@
 package LanesIntersection;
 
 import Components.PetriNet;
+import Components.PetriNetWindow;
 import Components.PetriTransition;
+import com.sun.tools.javac.Main;
 
 public class MainModel {
 
     public static void start(){
 
         PetriNet pn = new PetriNet();
+        System.out.println("Lanes Intersection!");
+
+        pn.PetriNetName = "Lanes Intersection";
+        pn.NetworkPort = 1080;
+
+        RebreanuIntersection.generatePetriNet(pn);
+        MacedonskiIntersection.generatePetriNet(pn);
+        Vuia1Intersection.generatePetriNet(pn);
+        Vuia2Intersection.generatePetriNet(pn);
+        UlpiaIntersection.generatePetriNet(pn);
+        DrubetaIntersection.generatePetriNet(pn);
+
+        pn.Delay = 2000;
+
+        PetriNetWindow frame = new PetriNetWindow(false);
+        frame.petriNet = pn;
+        frame.setVisible(true);
     }
 
 }
